782ed9f63a723f7222648a9131217bb3ed8ece46
Danny Monge <danny.monge.dev@gmail.com>
2016-11-10 23:50:05 +0200

    Add notification factories

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :notification do
-    phone "MyString"
-    body "MyText"
-    source_app "MyString"
+    phone '221-7878-2666'
+    body 'Body text'
+    source_app 'App name'
+  end
+
+  factory :other_notification, class: 'Notification' do
+    phone '443-9999-7777'
+    body 'Other body text'
+    source_app 'Other app name'
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Notification, type: :model do
   describe 'creation' do
     it 'can be created' do
-      notification = Notification.new(phone: '233-2211-4466', body: 'Body text', source_app: 'app name')
+      notification = create(:notification) # create method it can lead to a slow test suite.
       expect(notification).to be_valid
     end
   end
